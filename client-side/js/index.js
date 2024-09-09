fdd6310b13d040da25e35d5ed7e76f72edfef246
safa <safakallianthodi6@gmail.com>
2024-09-09 10:20:47 +0530

    Added

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,194 @@
-alert("hello")
+async function fetchData(){
+    const res =await fetch("http://localhost:3000/getEmployee")
+    console.log(res);
+    
+    const data= await res.json()
+    console.log(data);
+    str=``
+    data.map((employee)=>{
+        str+=`  <div>
+                        
+                        <tr>
+                            <td><input type="text"  name="ID" disabled=true placeholder="" value=${employee.ID} id="ID-${employee.ID}">
+                            </td>
+                            <td><input type="text" name="name" disabled=true placeholder="" value=${employee.name} id="name-${employee.ID}"></td>
+                            <td><input type="text" name="designation" disabled=true placeholder="" value=${employee.designation} id="designation-${employee.ID}"></td>
+                            <td><input type="text" name="salary" disabled=true placeholder=" " value=${employee.salary} id="salary-${employee.ID}"></td>
+                            <td><input type="text" name="experience" disabled=true placeholder="" value=${employee.experience} id="experience-${employee.ID}">
+                            </td>
+                            <td><div class="bonus" id="bonous-salary-${employee.ID}"> </div></td>
+                            <td> <button class="edit-btn" onclick="handleEdit('${employee.ID}')" >EDIT</button>
+                                <button class="save-btn" onclick="handleSave('${employee.ID}')" >SAVE</button>
+                                <button class="delete-btn" onclick="handleDelete('${employee.ID}')"  >DELETE</button></td>
+
+                        </tr>
+                </div>
+                        
+                        
+                     
+                   
+    `
+
+    })
+    document.getElementById("main").innerHTML=str
+
+    let salary=0;
+    data.map((employee)=>{
+        // console.log(employee.experience);
+        if(employee.experience==0){
+        //    console.log("hehe");
+           
+            document.getElementById(`bonous-salary-${employee.ID}`).textContent="INELIGIBLE"
+            document.getElementById(`bonous-salary-${employee.ID}`).style.color="red"
+            document.getElementById(`bonous-salary-${employee.ID}`).style.fontSize=12+"px"
+            document.getElementById(`bonous-salary-${employee.ID}`).style.fontWeight="bold"
+
+
+        }
+         else if(employee.experience==1){
+            salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.1)
+            document.getElementById(`bonous-salary-${employee.ID}`).textContent=salary
+
+        }
+        else if(employee.experience==2){
+            salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.15)
+            document.getElementById(`bonous-salary-${employee.ID}`).textContent=salary
+
+
+        }
+        else{
+            salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.2)
+            document.getElementById(`bonous-salary-${employee.ID}`).textContent=salary
+
+
+        }
+        console.log(salary);
+        console.log(employee.salary);
+        
+        
+        
+        
+
+    });
+
+    
+}
+
+fetchData()
+
+//FILTER aka Search code
+
+
+document.getElementById("search").addEventListener("keyup",async(e)=>{
+    try{
+        const res=await fetch("http://localhost:3000/getEmployee")
+        const data=await res.json()
+        const search =data.filter((employee)=>employee.name.toLowerCase().startsWith(e.target.value.toLowerCase()))
+        console.log(data);
+        console.log(search);
+        
+        str=``
+        search.map((employee)=>{
+          str+=`  <div>
+                        <tr>
+                            <td><input type="text"  name="ID" disabled=true placeholder="" value=${employee.ID} id="ID-${employee.ID}">
+                            </td>
+                            <td><input type="text" name="name" disabled=true placeholder="" value=${employee.name} id="name-${employee.ID}"></td>
+                            <td><input type="text" name="designation" disabled=true placeholder="" value=${employee.designation} id="designation-${employee.ID}"></td>
+                            <td><input type="text" name="salary" disabled=true placeholder=" " value=${employee.salary} id="salary-${employee.ID}"></td>
+                            <td><input type="text" name="experience" disabled=true placeholder="" value=${employee.experience} id="experience-${employee.ID}">
+                            </td>
+                             <td><div class="bonus" id="bonous-salary-${employee.ID}"> </div></td>
+                            <td> <button class="edit-btn" onclick="handleEdit('${employee.ID}')" >EDIT</button>
+                                <button class="save-btn" onclick="handleSave('${employee.ID}')" >SAVE</button>
+                                <button class="delete-btn" onclick="handleDelete('${employee.ID}')"  >DELETE</button>
+                            </td>
+                        </tr>
+                </div>
+          `
+
+        })
+        document.getElementById("main").innerHTML=str
+
+        let salary=0;
+        data.map((employee)=>{
+            // console.log(employee.experience);
+            if(employee.experience==0){
+                document.getElementById(`bonous-salary-${employee.ID}`).textContent="Hel"
+            }
+             else if(employee.experience==1){
+                salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.1)
+            }
+            else if(employee.experience==2){
+                salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.15)
+    
+            }
+            else{
+                salary=parseInt(employee.salary)+(parseInt(employee.salary)*0.2)
+    
+            }
+            console.log(salary);
+            console.log(employee.salary);
+            
+            document.getElementById(`bonous-salary-${employee.ID}`).textContent=salary
+    
+            
+            
+    
+        });
+    }
+   
+    catch(error){
+        console.log(error);
+   }
+});
+
+
+
+async function handleEdit(id) {
+    // console.log(id);
+    // document.getElementById(`ID-${id}`).disabled=false
+    document.getElementById(`name-${id}`).disabled=false
+    document.getElementById(`designation-${id}`).disabled=false
+    document.getElementById(`salary-${id}`).disabled=false
+    document.getElementById(`experience-${id}`).disabled=false   
+    
+}
+
+async function handleSave(id){
+    // console.log(id);
+    let name = document.getElementById(`name-${id}`).value
+    let designation = document.getElementById(`designation-${id}`).value
+    let salary = document.getElementById(`salary-${id}`).value
+    let experience = document.getElementById(`experience-${id}`).value
+    console.log(name,designation,salary,experience);
+    let objData ={id,name,designation,salary,experience}
+    console.log(objData);
+    const jsonData=JSON.stringify(objData)
+    console.log(jsonData);
+    const res =await fetch("http://localhost:3000/update",{
+        method:"PUT",
+        "Content-Type":"text/json",
+        body:jsonData
+    })
+
+    const message = await res.text()
+    // console.log(message);
+    res.status==200?alert(message):alert(message);
+    fetchData() 
+}
+
+async function handleDelete(id){
+    // console.log(id);
+    const res = await fetch("http://localhost:3000/delete",{
+        method:"delete",
+        "Content-Type":"text/plain",
+        body:id
+    })
+    const message = await res.text()
+    // console.log(message);
+    res.status==200?alert(message):alert(message);
+    fetchData() 
+    
+}
+
--- server-side/server.js
+++ server-side/server.js
@@ -1,29 +1,170 @@
 const http= require ("http")
 const fs= require ("fs")
 const url= require ("url")
+const queryString= require("querystring")
+const {MongoClient,ObjectId}=require ("mongodb")
 
-const app=http.createServer((req,res)=>{
+const PORT=3000;
+
+const client=  new MongoClient("mongodb://127.0.0.1:27017/")
+
+
+
+
+const app=http.createServer(async(req,res)=>{
     // console.log(typeof(req.url));
     const path = url.parse(req.url)
-    // console.log(path);
+
+    console.log(path);
+     //create database
+     const db = client.db("Employee")
+     //create collection
+     const collection =db.collection("staff")
 
     if(path.pathname=="/"){
         res.writeHead(200,{"Content-Type":"text/html"});
         res.end(fs.readFileSync("../client-side/index.html"))
     }
 
-    else if(path.pathname=="/css/index.css"){
+    else if(path.pathname=="/client-side/css/index.css"){
         res.writeHead(200,{"Content-Type":"text/css"});
         res.end(fs.readFileSync("../client-side/css/index.css"));
 
     }
+    else if(path.pathname=="/client-side/js/index.js"){
+        res.writeHead(200,{"Content-Type":"text/js"});
+        res.end(fs.readFileSync("../client-side/js/index.js"));
 
-    
-    
+    }
+    else if(path.pathname=="/add"){
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(fs.readFileSync("../client-side/pages/add.html"));
+
+    }
+    else if(path.pathname=="/css/add.css"){
+        res.writeHead(200,{"Content-Type":"text/css"});
+        res.end(fs.readFileSync("../client-side/css/add.css"));
+
+    }
+    console.log(req.method);
+
+    //add Data
+    if(path.pathname=="/submit" && req.method=="POST")  {
+       let body=""
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+            body+=chunk.toString();
+            console.log(body);
+        });
+        req.on("end",async()=>{
+            if(body!=null){
+                const formData=queryString.parse(body)
+                console.log(formData);
+                // console.log(formData.ID);
+                const existingData=await collection.findOne({ID:formData.ID})
+                if(existingData){
+                    console.log("Data with ID already exists");
+                   
+                }
+                else{
+                    collection.insertOne(formData).then(()=>{
+                        console.log("Success");
+                        
+                    }).catch((error)=>{
+                        console.log(error);
+                        
+                    })    
+
+                }
+                 
+            }
+        })
+        // redirect the page into homepage after submitting
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(fs.readFileSync("../client-side/index.html"))
+
+    }  
+    //get data from database and  send to the frontend
+
+    if(path.pathname=="/getEmployee" && req.method=="GET"){
+        const data = await collection.find().toArray()
+        console.log(data);
+        const jsonData=JSON.stringify(data)
+        console.log(jsonData);
+        res.writeHead(200,{"Content-Type":"text/json"});
+        res.end(jsonData)
+        
+    }
+
+    //save
+    if(path.pathname=="/update" && req.method=="PUT"){
+        console.log("Update route reached");
+        body=""
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+            body+=chunk.toString()
+            console.log(body);
+        });
+        req.on("end",async()=>{
+            let udata = JSON.parse(body)
+            console.log(udata);
+            const updateData= {name:udata.name,designation:udata.designation,salary:udata.salary,experience:udata.experience}
+            let ID=udata.id;
+            console.log(ID);
+            
+            await collection.updateOne({ID},{$set:updateData})
+
+            .then(()=>{
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.end("Updated Successfully")
+            }).catch(()=>{
+                res.writeHead(404,{"Content-Type":"text/html"})
+                res.end("Failed to update")
+            })
+        })
+        
+    }
+
+    //delete
+    if(path.pathname=="/delete" && req.method=="DELETE"){
+        console.log("You reached the delete route");
+
+        body=""
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+            body+=chunk.toString()
+            console.log(body);
+        });
+        req.on("end",async()=>{
+           
+            let ID=body;
+            collection.deleteOne({ID})
+            .then(()=>{
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.end("Deleted Successfully")
+            }).catch(()=>{
+                res.writeHead(404,{"Content-Type":"text/html"})
+                res.end("Failed to delete")
+            })
+        })
+    }
+
 
 })
 
-app.listen(3000)
+client.connect().then(()=>{
+    console.log("Database Connected");
+    app.listen(PORT,()=>{
+        console.log(`http://localhost:${PORT}`);
+        
+
+    })
+
+    
+}).catch((error)=>{
+        console.log(error);
+        
+    })
 
 
 
