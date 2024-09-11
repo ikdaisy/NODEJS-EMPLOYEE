3363a131d0fc3e0f80519ac1200520d2f717bebe
safa <safakallianthodi6@gmail.com>
2024-09-11 11:01:13 +0530

    Updated

--- client-side/js/add.js
+++ client-side/js/add.js
@@ -1,20 +1,38 @@
-alert("hello")
-
-document.getElementById("ID").addEventListener("keyup",async (e)=>{
-    console.log(e.target.value);
+ async function displayErrorMessage(){
     const res=await fetch("http://localhost:3000/getEmployee")
     // console.log(res);
     const data =await res.json()
     // console.log(data);
-    // let search=data.find(){
-    //     return e.target.value == "data.ID"
-    //     alert("Already exist")
-    // }
-    
-    
+    document.getElementById("ID").addEventListener("keyup",async (e)=>{
+        const enteredID= e.target.value.toUpperCase()
+        console.log(enteredID)
+        const existingData=data.find((employee)=>{
+            // console.log(employee.ID);
+            // console.log(e.target.value);
+            
+            
+            return employee.ID===enteredID
+        })
+        //find method returns undefined if there's no match is found
+        //undefined value is same as false 
+        // console.log(exisitingData);
 
-  
+        errorMessageElement=document.getElementById("errorMessage");
+        if (existingData) {
+            errorMessageElement.textContent = "ID already exists!";
+            errorMessageElement.style.display = "block";
+            errorMessageElement.style.color="red"
+            errorMessageElement.style.fontWeight = "bold";
+            errorMessageElement.style.fontSize = 12+"px";
 
-    
+           document.getElementById("add-btn").disabled=true;
 
-})
+          } else {
+            errorMessageElement.textContent = "";
+            errorMessageElement.style.display = "none";
+          }
+            
+    });
+ }
+
+ displayErrorMessage();
